ffc17ff2c40c65ea46e31b8971e03789be973c50
Samuel Perez Besada <samusitesi@gmail.com>
2025-03-07 17:11:09 +0100

    DB and server created

---
+++ server.js
@@ -0,0 +1,26 @@
+const jsonServer = require("json-server")
+
+const server = jsonServer.create() // crea el código del servidor json
+
+// middlewares son configuraciones de el servidor
+const middlewares = jsonServer.defaults() // crea las configuraciones esenciales de el server
+
+server.use(middlewares) // hacer uso de las configuraciones
+
+const allowAccessFromAnywhere = (req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*")
+  next()
+}
+
+server.use(allowAccessFromAnywhere) // esto permite que cualquier cliente pueda acceder a mi servidor
+
+const router = jsonServer.router("db.json")
+
+server.use(router) // esto crea TODAS las rutas basicas de acceso y modificacion a la DB (get, post, patch, put, delete)
+
+const PORT = 5005
+
+server.listen(PORT, () => {
+  console.log("Servidor andando")
+  console.log(`Ejecutandose localmente en http://localhost:${PORT}`)
+}) // podemos a andar el servidor y que escuche continuamente llamadas de clientes.
